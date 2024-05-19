aedb097232f7b439bfc48dd6407f088e846aee56
ibrahim Tuna <tuna.cort52@gmail.com>
2024-05-19 13:45:21 +0300

    Model, migration ve foreign keyler tamamlandı

---
+++ database/migrations/2024_05_19_091637_create_cars_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('model_id');
+            $table->unsignedBigInteger('damage_id');
+            $table->unsignedInteger('year');
+            $table->string('color');
+            $table->unsignedInteger('km');
+            $table->boolean('garanti_status')->default(0);
+            $table->tinyInteger('vites_turu')->comment('0-mauel 1-otomatik 2-yarı otomatik');
+            $table->tinyInteger('yakit_turu')->comment('0-benzin 1-dizel 2-lpg');
+            $table->dateTime('announcment_date');
+            $table->tinyInteger('status')->comment('0-pasif 1-aktif 2-satıldı');
+            $table->integer('fiyat');
+            $table->text('description')->nullable();
+
+
+
+
+            $table->softDeletes();
+            $table->timestamps();
+
+
+            $table->foreign('user_id')
+                ->on('users')->references('id')->onDelete('cascade');
+
+            $table->foreign('model_id')
+                ->on('car_models')->references('id')->onDelete('cascade');
+
+            $table->foreign('damage_id')
+                ->on('car_damages')->references('id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
