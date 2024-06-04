69de1e97fbbc09981b94b3ab9bc95fad0a72b63d
ibrahim Tuna <tuna.cort52@gmail.com>
2024-06-04 16:52:54 +0300

    Routelar güncellendi

--- resources/views/panel/layouts/app.blade.php
+++ resources/views/panel/layouts/app.blade.php
@@ -117,7 +117,15 @@
 <!-- Modal Search End -->
 
 <div class="container-fluid page-header py-5">
-    <h1 class="text-center text-white display-6">Shop Detail</h1>
+    <h1 class="text-center text-white display-6">
+        @if(request()->routeIs('cars.index'))
+            Home
+        @elseif(request()->routeIs('cars.create'))
+            İlan Ver
+        @elseif(route('admin.brand.index'))
+            Markalar
+        @endif
+        </h1>
     <ol class="breadcrumb justify-content-center mb-0">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
         <li class="breadcrumb-item"><a href="#">Pages</a></li>
